823cdf4844faec9207297df873de239c8b44797d
Jake Reck <jakereck@gmail.com>
2025-05-13 08:06:34 -0500

    Applying changes

--- main.tf
+++ main.tf
@@ -26,3 +26,64 @@ resource "aws_internet_gateway" "mtc_igw" {
     Name = "dev-igw"
   }
 }
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mtc_igw.id
+}
+
+resource "aws_route_table_association" "mtc_public_assoc" {
+  subnet_id      = aws_subnet.mtc_public_subnet.id
+  route_table_id = aws_route_table.mtc_public_rt.id
+}
+
+resource "aws_security_group" "mtc_sg" {
+  name        = "dev-sg"
+  description = "Dev security group"
+  vpc_id      = aws_vpc.mtc_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["99.160.142.12/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "mtc_auth" {
+  key_name   = "mtckey"
+  public_key = file("~/.ssh/mtckey.pub")
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.server_ami.id
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  key_name               = aws_key_pair.mtc_auth.key_name
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
+
+  root_block_device {
+    volume_size = 10
+  }
+}
